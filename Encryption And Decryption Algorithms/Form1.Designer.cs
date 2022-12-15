ec867a44474ddb5a06d4cdb9a37196affa9865a3
يارب <@DESKTOP-GHSHA8O>
2022-12-16 00:40:58 +0200

    Add RSA

--- Encryption And Decryption Algorithms/Form1.Designer.cs
+++ Encryption And Decryption Algorithms/Form1.Designer.cs
@@ -37,13 +37,18 @@ namespace Encryption_And_Decryption_Algorithms
             this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(312, 82);
+            this.textBox1.Location = new System.Drawing.Point(243, 82);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(216, 96);
@@ -52,7 +57,7 @@ namespace Encryption_And_Decryption_Algorithms
             // label1
             // 
             this.label1.ForeColor = System.Drawing.Color.Black;
-            this.label1.Location = new System.Drawing.Point(309, 46);
+            this.label1.Location = new System.Drawing.Point(240, 46);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(173, 23);
             this.label1.TabIndex = 1;
@@ -60,7 +65,7 @@ namespace Encryption_And_Decryption_Algorithms
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(41, 82);
+            this.textBox2.Location = new System.Drawing.Point(21, 82);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(201, 96);
@@ -69,7 +74,7 @@ namespace Encryption_And_Decryption_Algorithms
             // label2
             // 
             this.label2.ForeColor = System.Drawing.Color.Black;
-            this.label2.Location = new System.Drawing.Point(38, 46);
+            this.label2.Location = new System.Drawing.Point(18, 46);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(173, 23);
             this.label2.TabIndex = 1;
@@ -77,15 +82,15 @@ namespace Encryption_And_Decryption_Algorithms
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(163, 289);
+            this.textBox3.Location = new System.Drawing.Point(131, 285);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(221, 20);
+            this.textBox3.Size = new System.Drawing.Size(180, 20);
             this.textBox3.TabIndex = 2;
             // 
             // label4
             // 
             this.label4.ForeColor = System.Drawing.Color.Black;
-            this.label4.Location = new System.Drawing.Point(123, 289);
+            this.label4.Location = new System.Drawing.Point(128, 269);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(34, 23);
             this.label4.TabIndex = 1;
@@ -93,61 +98,117 @@ namespace Encryption_And_Decryption_Algorithms
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(69, 350);
+            this.button1.BackColor = System.Drawing.Color.GreenYellow;
+            this.button1.Location = new System.Drawing.Point(165, 396);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 3;
             this.button1.Text = "Encrypte";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.Encryptr_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(424, 350);
+            this.button2.BackColor = System.Drawing.Color.ForestGreen;
+            this.button2.Location = new System.Drawing.Point(165, 425);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
             this.button2.Text = "Decrypte";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.Decrypte_Click);
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(213, 223);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(105, 17);
-            this.checkBox1.TabIndex = 4;
-            this.checkBox1.Text = "Caesar Cipher";
-            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(228, 350);
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.Location = new System.Drawing.Point(165, 454);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 5;
             this.button3.Text = "Clear";
-            this.button3.UseVisualStyleBackColor = true;
+            this.button3.UseVisualStyleBackColor = false;
             this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(128, 347);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 23);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Q";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(131, 361);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(180, 20);
+            this.textBox4.TabIndex = 2;
+            // 
+            // label5
+            // 
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(128, 308);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 23);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "P";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(131, 324);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(180, 20);
+            this.textBox5.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.comboBox1.DisplayMember = "1";
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "RCA",
+            "Caeser Cipher"});
+            this.comboBox1.Location = new System.Drawing.Point(131, 222);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(180, 21);
+            this.comboBox1.TabIndex = 6;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(153, 196);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(124, 23);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Choose algorithm";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Tomato;
-            this.ClientSize = new System.Drawing.Size(578, 460);
+            this.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.ClientSize = new System.Drawing.Size(488, 489);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button3);
-            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Font = new System.Drawing.Font("Cooper Black", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Encryption Algorithms";
@@ -166,8 +227,13 @@ namespace Encryption_And_Decryption_Algorithms
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Encryption And Decryption Algorithms/Form1.cs
+++ Encryption And Decryption Algorithms/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography;
 
 namespace Encryption_And_Decryption_Algorithms
 {
@@ -16,8 +17,10 @@ namespace Encryption_And_Decryption_Algorithms
         {
             InitializeComponent();
         }
+      static  bool Cipher = false;
+        static bool RSA = true;
 
-
+        //Caesar Cipher 
         public static char cipher(char ch, int key)
         {
             if (!char.IsLetter(ch))
@@ -30,28 +33,79 @@ namespace Encryption_And_Decryption_Algorithms
             return (char)((((ch + key) - d) % 26) + d);
 
         }
+        //Choose e
+        public static double get_e(double a, double h)
+        {
+            double temp;
+            while (true)
+            {
+                temp = a % h;
+                if (temp == 0)
+                    return h;
+                a = h;
+                h = temp;
+            }
+        }
+       public static double c;
+       public static double d;
+       public static double n;
+     
+
+
 
 
         private void Encryptr_Click(object sender, EventArgs e)
         {
-            if (textBox3.Text == "")
+            if (Cipher == true)
             {
-                MessageBox.Show("please Enter the Key");
-            }
-            else
-            {
-                var text = textBox2.Text;
-                var key = int.Parse(textBox3.Text);
-                string output = string.Empty;
-                foreach (char ch in text)
-                    output += cipher(ch, key);
+                if (textBox3.Text == "")
+                {
+                    MessageBox.Show("please Enter the Key");
+                }
+                else
+                {
+                    var text = textBox2.Text;
+                    var key = int.Parse(textBox3.Text);
+                    string output = string.Empty;
+                    foreach (char ch in text)
+                        output += cipher(ch, key);
 
-                textBox1.Text = output;
+                    textBox1.Text = output;
+                }
+            }else if (RSA == true)
+            {
+                if (textBox4.Text == "" || textBox5.Text == "")
+                {
+                    MessageBox.Show("please Enter the Keys");
+                }
+                else { 
+                double p =double.Parse( textBox5.Text);
+                double q = double.Parse( textBox4.Text);
+                 n = p * q;
+                double _e = 2;
+                double phi = (p - 1) * (q - 1);
+                while (_e < phi)
+                {                  
+                    if (get_e(_e, phi) == 1)
+                        break;
+                    else
+                        _e++;
+                }
+                int k = 2; 
+                  d = (1 + (k * phi)) / _e;
+                var msg = textBox2.Text;
+                textBox1.Text = String.Format("{0:F6}", msg);
+                c = Math.Pow(double.Parse( msg), _e);
+                c = c % n;
+                textBox1.Text= String.Format("{0:F6}", c);
+            }
             }
 
         }
+        
         private void Decrypte_Click(object sender, EventArgs e)
         {
+            if (Cipher == true) { 
             if (textBox3.Text == "")
             {
                 MessageBox.Show("please Enter the Key");
@@ -66,6 +120,13 @@ namespace Encryption_And_Decryption_Algorithms
 
                 textBox2.Text = output;
             }
+            }else if (RSA == true)
+            {
+
+                double m = Math.Pow(c, d);
+                m = m % n;
+                textBox2.Text= String.Format("{0:F6}", m);
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -73,8 +134,28 @@ namespace Encryption_And_Decryption_Algorithms
             textBox1.Text = "";
             textBox2.Text = "";
             textBox3.Text = "";
+            textBox4.Text = "";
+            textBox5.Text = "";
 
 
+
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           int currentMyComboBoxIndex = comboBox1.FindStringExact(comboBox1.Text);
+            if (currentMyComboBoxIndex == 0)
+            {
+                RSA = true;
+                Cipher = false;
+
+            }
+            else if(currentMyComboBoxIndex == 1)
+            {
+                RSA = false;
+                Cipher = true;
+            }
         }
     }
 }
